2080e5e4a6c9e6c98125f4820d289e807b2ac808
Mangesh Balkawade <101330511+mangesh-balkawade@users.noreply.github.com>
2022-08-07 14:21:12 +0530

    Delete 24_MarvellousModule.js

--- 24_MarvellousModule.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var _23_MarvellousModule_1 = require("./23_MarvellousModule");
-var empObj = new _23_MarvellousModule_1.Employee("Piyush", 11);
-empObj.displayEmployee();
